de71324052a10d2d3193ba078f31a7f7386f1ec2
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2021-10-27 23:33:21 +0200

    Add files via upload

---
+++ int_in_base.py
@@ -0,0 +1,59 @@
+# This class represents a number expressed as the digits in it's own base
+# e.g. having base 2, digits [1, 0, 0, 1] it will display decimal_value() as 9
+# Use convert_otherbase(...) to get the digits in the other base
+class int_in_base():
+    def __init__(self, pbase):
+        if type(pbase) != int:
+            raise Exception("An integer was expected.")
+        if pbase < 2:
+            raise Exception("An integer >= 2 was expected.")
+        self.base = pbase
+        self.digit = [0]
+
+    def decimal_value(self):
+        nd = len(self.digit)
+        n = 0
+
+        for i in range(0, nd):
+            n += self.digit[i] * (self.base ** (nd - (i + 1)))
+        return n
+    
+    def convert_otherbase(self, potherbase):
+        if type(potherbase) != int:
+            raise Exception("An integer was expected.")
+        if potherbase < 2:
+            raise Exception("An integer >= 2 was expected.")
+
+        result = int_in_base(potherbase)
+        result.digit.clear()
+
+        n = self.decimal_value()
+
+        if n == 0:
+            return [0]
+        
+        nd = 0
+        while potherbase ** (nd + 1) <= n:
+            nd += 1
+
+        j = 0
+
+        while j <= nd:
+            k = n // (potherbase ** (nd - j))
+            result.digit.append(k)
+            n -= k * (potherbase ** (nd - j))
+            j += 1
+        
+        return result
+
+    def set_base(self, pbase):
+        self.__init__(pbase)
+
+    def set_digits(self, *digits):
+        self.digit = []
+        for dg in digits:
+            if type(dg) != int:
+                raise Exception("A list of integers was expected.")
+            if dg > self.base - 1 or dg < 0:
+                raise Exception("Each digit should be >= 0 and <= base - 1.")
+            self.digit.append(dg)
---
+++ numeralsystemconverter_main.py
@@ -0,0 +1,19 @@
+from int_in_base import int_in_base
+
+# The int_in_base class contains the methods to convert between numeral systems
+
+# We can create an int_in_base object with any base
+a = int_in_base(89)
+
+# We can alter the base of an existing int_in_base object instance
+a.set_base(29)
+
+# set_digits is used to assign a value, each argument being the respective digit (the digit itself is 10-based!)
+a.set_digits(4, 19, 28, 1, 13, 7)
+
+# The decimal_value method specifically converts to decimal system
+print(str(a.decimal_value())) # expected output 96167052
+
+# The convert_otherbase method returns another int_in_base object instance, having the same value in the new base
+b = a.convert_otherbase(31)
+print(b.digit) # expected output [3, 11, 4, 1, 23, 30]
